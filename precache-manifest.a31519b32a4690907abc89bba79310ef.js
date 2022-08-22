d01f601b970e335dc98502f6694d701b88b92f69
hahays <diggabeat@gmail.com>
2022-08-22 18:39:24 +0300

    Updates

---
+++ precache-manifest.a31519b32a4690907abc89bba79310ef.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2549fe06bf1b5b2098de",
+    "url": "/Game-Box/static/css/main.16d02c9a.chunk.css"
+  },
+  {
+    "revision": "2549fe06bf1b5b2098de",
+    "url": "/Game-Box/static/js/main.2549fe06.chunk.js"
+  },
+  {
+    "revision": "2d6efd79d3ea59b7cb09",
+    "url": "/Game-Box/static/css/1.a4d8ea2c.chunk.css"
+  },
+  {
+    "revision": "2d6efd79d3ea59b7cb09",
+    "url": "/Game-Box/static/js/1.2d6efd79.chunk.js"
+  },
+  {
+    "revision": "43e86a1d5560ff307c66",
+    "url": "/Game-Box/static/js/runtime~main.43e86a1d.js"
+  },
+  {
+    "revision": "7745ee58a9b136926b7c266580da711a",
+    "url": "/Game-Box/static/media/blank.7745ee58.png"
+  },
+  {
+    "revision": "4c4974dc343e48b41a3daecaec35bc44",
+    "url": "/Game-Box/static/media/breakOut.4c4974dc.png"
+  },
+  {
+    "revision": "e5dfcc28aae95521af0c95d190a12ac2",
+    "url": "/Game-Box/static/media/connectFour.e5dfcc28.png"
+  },
+  {
+    "revision": "db2f0800df65afc3734896ef763a616f",
+    "url": "/Game-Box/static/media/frogger.db2f0800.png"
+  },
+  {
+    "revision": "aa06187f050d9b1122d636435ee49207",
+    "url": "/Game-Box/static/media/snake.aa06187f.png"
+  },
+  {
+    "revision": "71378b6b9037d1e3a0b2cabb888d4ffa",
+    "url": "/Game-Box/static/media/memoryGame.71378b6b.png"
+  },
+  {
+    "revision": "2a8dd960380712549a459c9ae44d9ee6",
+    "url": "/Game-Box/static/media/spaceInvaders.2a8dd960.png"
+  },
+  {
+    "revision": "ed7c265fa1d9a08f8653a7ba56bfd202",
+    "url": "/Game-Box/static/media/BdNew4.ed7c265f.png"
+  },
+  {
+    "revision": "1dfd08384db2bc969c514545403bd3bf",
+    "url": "/Game-Box/static/media/gamebox.1dfd0838.png"
+  },
+  {
+    "revision": "1f968a5303b4821d31649463f5ef11be",
+    "url": "/Game-Box/static/media/televizor2.1f968a53.png"
+  },
+  {
+    "revision": "810aee48362a95322ccb34def91cdd7e",
+    "url": "/Game-Box/static/media/televizor1.810aee48.png"
+  },
+  {
+    "revision": "62cb9536d451e2eb109fceb2cbe4abce",
+    "url": "/Game-Box/static/media/televizor3.62cb9536.png"
+  },
+  {
+    "revision": "89b826dbe90ada368802774377d19357",
+    "url": "/Game-Box/static/media/console.89b826db.png"
+  },
+  {
+    "revision": "01de8b2c1329e18ceef03470e1117d22",
+    "url": "/Game-Box/index.html"
+  }
+];
